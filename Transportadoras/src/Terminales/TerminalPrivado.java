9b485a69fe5b2ef9b6a090bdf3dd114a4c02be47
Yeinson Valencia <yvcdev@gmail.com>
2023-07-11 20:27:45 -0500

    diagrama de clases

--- Transportadoras/src/Terminales/TerminalPrivado.java
+++ Transportadoras/src/Terminales/TerminalPrivado.java
@@ -1,5 +1,32 @@
 package Terminales;
 
-public class TerminalPrivado {
-    
+import java.util.ArrayList;
+
+public class TerminalPrivado extends Terminal {
+    private ArrayList<String> empresas;
+    private int numEmpresa;
+
+    public TerminalPrivado(String nombre, String ciudad, String departamento, String pais, int numCompania) {
+        super(nombre, ciudad, departamento, pais, numCompania);
+        this.empresas = new ArrayList<String>();
+        this.numEmpresa = 0;
+    }
+
+    public TerminalPrivado(String nombre, String ciudad, String departamento, String pais, int numCompania,
+            ArrayList<Compania> listaCompanias, ArrayList<String> empresas) {
+        super(nombre, ciudad, departamento, pais, numCompania, listaCompanias);
+        this.empresas = empresas;
+    }
+
+    public void insertarEmpresa(String empresa) {
+        this.empresas.add(empresa);
+    }
+
+    public void insertarEmpresas(ArrayList<String> empresas) {
+        this.empresas.addAll(empresas);
+    }
+
+    public ArrayList<String> getEmpresas() {
+        return empresas;
+    }
 }
--- Transportadoras/src/Terminales/TerminalPublico.java
+++ Transportadoras/src/Terminales/TerminalPublico.java
@@ -1,5 +1,22 @@
 package Terminales;
 
-public class TerminalPublico {
-    
+import java.util.ArrayList;
+
+public class TerminalPublico extends Terminal {
+    private double recursosPublicos;
+
+    public TerminalPublico(String nombre, String ciudad, String departamento, String pais, int numCompania) {
+        super(nombre, ciudad, departamento, pais, numCompania);
+    }
+
+    public TerminalPublico(String nombre, String ciudad, String departamento, String pais, int numCompania,
+            ArrayList<Compania> listaCompanias,
+            double recursosPublicos) {
+        super(nombre, ciudad, departamento, pais, numCompania, listaCompanias);
+        this.recursosPublicos = recursosPublicos;
+    }
+
+    public double getRecursosPublicos() {
+        return recursosPublicos;
+    }
 }
