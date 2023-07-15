6667db8974d8dfb5edf250a66feb05be4b34f69f
Yeinson Valencia <yvcdev@gmail.com>
2023-07-15 13:09:33 -0500

    Inicio metodo en main

---
+++ Transportadoras/src/Terminales/Consultas.java
@@ -0,0 +1,9 @@
+package Terminales;
+
+import javax.swing.JOptionPane;
+
+public class Consultas {
+    public static void consultarTerminales() {
+        JOptionPane.showMessageDialog(null, "Consultar terminales");
+    }
+}
