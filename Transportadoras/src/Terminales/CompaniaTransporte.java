c905ce0911bda269c5b28c44f188aa66e76394ed
feanve <felipe.velez@ucp.edu.co>
2023-07-15 10:01:26 -0500

    Clases utilities actualizadas

--- Transportadoras/src/Terminales/CompaniaTransporte.java
+++ Transportadoras/src/Terminales/CompaniaTransporte.java
@@ -2,7 +2,7 @@ package Terminales;
 
 public class CompaniaTransporte {
 
-    public CompaniaTransporte(String string, String string2, String string3, String string4, String string5) {
+    public CompaniaTransporte(String nit, String nombre, String nombre_corto, String departamento, String ciudad) {
     }
 
 }
--- Transportadoras/src/Terminales/TerminalTransporte.java
+++ Transportadoras/src/Terminales/TerminalTransporte.java
@@ -2,8 +2,8 @@ package Terminales;
 
 public class TerminalTransporte {
 
-    public TerminalTransporte(String string, String string2, String string3, String string4, String string5,
-            String string6) {
+    public TerminalTransporte(String consecutivo, String ciudad, String direccion, String resolucion, String gerente,
+            String codigo) {
     }
 
 }
