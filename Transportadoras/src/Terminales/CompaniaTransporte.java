c250c4f3f935c2c24f25c3445c74385dde92db41
Yeinson Valencia <yvcdev@gmail.com>
2023-07-16 18:06:00 -0500

    removed unused files

--- Transportadoras/src/Terminales/CompaniaTransporte.java
+++
@@ -1,8 +0,0 @@
-package Terminales;
-
-public class CompaniaTransporte {
-
-    public CompaniaTransporte(String nit, String nombre, String nombre_corto, String departamento, String ciudad) {
-    }
-
-}
